8ca38f9f9ef91e274228289d9441f979213c8fe4
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2021-11-02 11:07:20 +0100

    Add tests for iteration and lexeme prop querying

--- test/test_lexemes.py
+++ test/test_lexemes.py
@@ -15,3 +15,85 @@ class TestLexemes(unittest.TestCase):
         lex_id_2 = lexicon.add_lexeme("examples", "example", "NOUN")
 
         self.assertNotEqual(lex_id_1, lex_id_2)
+
+    def test_correct_props(self):
+        lexicon = SegLex()
+        lex_id_1 = lexicon.add_lexeme("example", "example", "NOUN")
+        lex_id_2 = lexicon.add_lexeme("examples", "example", "NOUN", {"number": "pl"})
+
+        self.assertEqual("example", lexicon.form(lex_id_1))
+        self.assertEqual("example", lexicon.lemma(lex_id_1))
+        self.assertEqual("NOUN", lexicon.pos(lex_id_1))
+        self.assertEqual({}, lexicon.features(lex_id_1))
+
+        self.assertEqual("examples", lexicon.form(lex_id_2))
+        self.assertEqual("example", lexicon.lemma(lex_id_2))
+        self.assertEqual("NOUN", lexicon.pos(lex_id_2))
+        self.assertEqual({"number": "pl"}, lexicon.features(lex_id_2))
+
+    def test_iter(self):
+        lexicon = SegLex()
+        lex_id_1 = lexicon.add_lexeme("example", "example", "NOUN")
+        lex_id_2 = lexicon.add_lexeme("example", "example", "NOUN")
+        lex_id_3 = lexicon.add_lexeme("examples", "example", "NOUN")
+        lex_id_4 = lexicon.add_lexeme("counterexample", "counterexample", "NOUN")
+        lex_id_5 = lexicon.add_lexeme("counterexample", "counterexample", "NOUN")
+        lex_id_6 = lexicon.add_lexeme("exemplar", "exemplar", "ADJ")
+
+        self.assertCountEqual([lex_id_1,
+                               lex_id_2,
+                               lex_id_3,
+                               lex_id_4,
+                               lex_id_5,
+                               lex_id_6],
+                              list(lexicon.iter_lexemes()))
+
+        self.assertCountEqual([lex_id_1,
+                               lex_id_2],
+                              list(lexicon.iter_lexemes(form="example")))
+
+        self.assertCountEqual([lex_id_1,
+                               lex_id_2,
+                               lex_id_3],
+                              list(lexicon.iter_lexemes(lemma="example")))
+
+        self.assertCountEqual([lex_id_4,
+                               lex_id_5],
+                              list(lexicon.iter_lexemes(lemma="counterexample")))
+
+        self.assertCountEqual([lex_id_4,
+                               lex_id_5],
+                              list(lexicon.iter_lexemes(lemma="counterexample", pos="NOUN")))
+
+        self.assertCountEqual([lex_id_4,
+                               lex_id_5],
+                              list(lexicon.iter_lexemes(form="counterexample", lemma="counterexample")))
+
+        self.assertCountEqual([lex_id_4,
+                               lex_id_5],
+                              list(lexicon.iter_lexemes(form="counterexample", lemma="counterexample", pos="NOUN")))
+
+        self.assertEqual([],
+                         list(lexicon.iter_lexemes(form="counterexample", lemma="counterexample", pos="ADJ")))
+
+        self.assertEqual([],
+                         list(lexicon.iter_lexemes(form="counterexamples", lemma="counterexample", pos="NOUN")))
+
+        self.assertEqual([],
+                         list(lexicon.iter_lexemes(form="counterexample", lemma="counterexamples", pos="NOUN")))
+
+        self.assertCountEqual([lex_id_1,
+                               lex_id_2,
+                               lex_id_3,
+                               lex_id_4,
+                               lex_id_5],
+                              list(lexicon.iter_lexemes(pos="NOUN")))
+
+        self.assertEqual([],
+                         list(lexicon.iter_lexemes(form="silly")))
+
+        self.assertEqual([],
+                         list(lexicon.iter_lexemes(lemma="silly")))
+
+        self.assertEqual([],
+                         list(lexicon.iter_lexemes(pos="VERB")))
