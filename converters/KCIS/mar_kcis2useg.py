58e10ecba294a713760a78fd7d28e4a746646ce4
Niyati Bafna <niyatibafna13@gmail.com>
2021-11-16 10:20:25 +0100

    Running on all Marathi files, logging errors, basic POS translation

--- converters/KCIS/mar_kcis2useg.py
+++ converters/KCIS/mar_kcis2useg.py
@@ -32,17 +32,50 @@ lexicon = SegLex()
 
 def assign_upos(pos):
     '''Maps POS to UPOS'''
-    return pos
+    upos = ""
+    if pos.startswith("N"):
+        upos = "NOUN"
+    if pos.startswith("NNP"):
+        upos = "PROPN"
+    if pos.startswith("NNP"):
+        upos = "NOUN"
+    if pos.startswith("V"):
+        upos = "VERB"
+    if "VAU" in pos or "VAU" in pos:
+        upos = "AUX"
+    if pos.startswith("X"):
+        upos = "X"
+    if pos.startswith("RP"):
+        upos = "PART"
+    if pos.startswith("J"):
+        upos = "JJ"
+    if pos.startswith("RB"):
+        upos = "ADV"
+    if pos.startswith("PR"):
+        upos = "ADP"
+
+    if upos=="":
+        pos_issues.warning("POS %s not assigned ", pos)
+
+    return upos
 
 def get_lemma(wordform, pos, fs):
     '''Returns lemma'''
     return wordform
 
-def get_lexeme_features(af):
+def get_lexeme_features(af, pos):
     '''Extracts and translates features from af'''
     features = dict()
     features["root"] = af[0]
-    #features["gender"] ...
+    if len(features)!=8:
+        return features
+    features["lcat"] = af[1]
+    features["gender"] = af[2]
+    features["number"] = af[3]
+    features["person"] = af[4]
+    features["case"] = af[6]
+    features["person"] = af[7]
+    features["AnnCorra_tag"] = pos
     return features
 
 
@@ -59,28 +92,32 @@ for line in infile:
 
     upos = assign_upos(pos)
     lemma = get_lemma(wordform, pos, fs)
-    features = get_lexeme_features(af)
+    features = get_lexeme_features(af, pos)
 
     lex_id = lexicon.add_lexeme(wordform, lemma, upos, features)
 
     if len(af)!=8:
-        print(af)
+        gen_issues.warning("Wordform %s has af: %s without enough fields", wordform, af)
         continue
 
     if af[7] != "":
         suffixes = af[7].split("_")
         suffixes.reverse()
         end = len(wordform)
-        for suffix in suffixes[:-1]:
+        # if len(suffixes)>1:
+            # suffixes = suffixes[:-1]
+        for suffix in suffixes:
             features = {"info":""}
             start = len(wordform[:end]) - len(suffix)
             if not wordform[:end].endswith(suffix):
-                print(wordform, suffix, af)
-                print(start, end)
+                seg_issues.warning("Suffix %s not at position %s, %s, of wordform %s, af: %s", suffix, start, end, wordform, af)
+                # print(wordform, suffix, af)
+                # print(start, end)
                 continue
 
             lexicon.add_contiguous_morpheme(lex_id, annot_name, start, end, features)
             end = start
+        lexicon.add_contiguous_morpheme(lex_id, annot_name, 0, end, features={"type":"root"})
 
 
 outfile = open(sys.argv[2], 'w')
