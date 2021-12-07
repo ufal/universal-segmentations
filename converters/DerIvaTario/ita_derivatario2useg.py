9461fa993e2fec7f66e715c23b037713aa50c1d1
Niyati Bafna <niyatibafna13@gmail.com>
2021-12-07 10:31:11 +0100

    Marking doubling, adding multiple entries per POS, adding ordering information in JSON such that original order can be inferred

--- converters/DerIvaTario/ita_derivatario2useg.py
+++ converters/DerIvaTario/ita_derivatario2useg.py
@@ -4,6 +4,7 @@ import re
 import sys
 sys.path.append('../../src/')
 from useg import SegLex
+from collections import defaultdict
 
 
 import logging
@@ -37,27 +38,32 @@ gen_issues.info(f"Converting {sys.argv[1]} to {sys.argv[3]}")
 def initialize_all_upos():
     '''Reads UDer file and converts to dictionary'''
     uder = open(sys.argv[2])
-    upos_assignment = dict()
+    upos_assignment = defaultdict(lambda: set())
     for line in uder:
         if line == "\n" or line == " ":
             continue
         word, upos = line.split("\t")[1].strip().split("#")
-        if word in upos_assignment and upos_assignment[word] != upos:
-            # logging.warning("Word %s has more than 1 POS: %s, %s ", word, upos_assignment[word], upos)
-            upos_assignment[word] = upos_assignment[word] + " , " + upos
-        else:
-            upos_assignment[word] = upos
+        upos_assignment[word].add(upos)
+
+        # if word in upos_assignment and upos_assignment[word] != upos:
+        #     # logging.warning("Word %s has more than 1 POS: %s, %s ", word, upos_assignment[word], upos)
+        #     upos_assignment[word] = upos_assignment[word] + " , " + upos
+        # else:
+        #     upos_assignment[word] = upos
 
     return upos_assignment
 
 
 def assign_upos(lexeme, upos_assignment):
     '''Finds lexeme in UDer and its POS'''
-    try:
-        return upos_assignment[lexeme]
-    except KeyError:
+
+    if len(upos_assignment[lexeme])==0:
         pos_issues.warning("Lexeme %s not in UDer, POS not found", lexeme)
-        return "UNK"
+        return "UNK", set()
+    #RETURNING SOME CHOSEN POS AS WELL THE REMAINING ELEMENTS
+    for pos in upos_assignment[lexeme]:
+        break
+    return pos, upos_assignment[lexeme] - {pos}
 
 def longest_common_prefix(s, t):
     '''Finds length of LCP'''
@@ -205,12 +211,13 @@ upos_assignment = initialize_all_upos()
 for line in infile:
     entries = [s.lower() for s in line.strip().split(";")]
     entries = list(filter(lambda f: f!="", entries))
+    colfis_id = entries[0]
     lexeme = entries[1]
     root = entries[2].split(":")
 
 
-    # if lexeme!="narcisisticamente".lower():
-    #     continue
+    # if lexeme!="ribassista".lower():
+        # continue
 
     if len(root) < 2:
         gen_issues.warning("Root %s does not have 2 fields", root)
@@ -218,9 +225,13 @@ for line in infile:
     if len(entries) <= 2:
         gen_issues.warning("Lexeme %s only has root", lexeme)
 
-    upos = assign_upos(lexeme, upos_assignment)
+    lex_features = dict()
+    lex_features["colfis_id"] = colfis_id
 
-    lex_features = {"upos": upos}
+    upos, all_upos = assign_upos(lexeme, upos_assignment)
+    # lex_features["upos"] = upos
+    # if all_upos:
+    #     lex_features["other_possible_upos"] = " , ".join(all_upos)
 
     if root[0] == "BASELESS":
         lex_features["root"] = "unrec"
@@ -231,148 +242,175 @@ for line in infile:
         if len(root) > 2:
             lex_features["complex_type"] = root[2]
 
+    for upos in all_upos.union({upos}):
 
-    lex_id = lexicon.add_lexeme(lexeme, lexeme, upos, features=lex_features)
+        lex_features["upos"] = upos
+        lex_id = lexicon.add_lexeme(lexeme, lexeme, upos, features=lex_features)
 
-    info_morphemes = entries[2:]
+        info_morphemes = entries[2:]
 
+        # Arranging morphemes according to order in wordform
+        morpheme_seq = []
+        ordering = list()
+        for idx, info_morpheme in enumerate(info_morphemes):
+            if info_morpheme.split(":")[0] in prefixes:
+                morpheme_seq = [info_morpheme] + morpheme_seq
+                ordering = [idx] + ordering
+            else:
+                morpheme_seq = morpheme_seq + [info_morpheme]
+                ordering = ordering + [idx]
 
-    # Arranging morphemes according to order in wordform
-    morpheme_seq = []
-    for info_morpheme in info_morphemes:
-
-        if info_morpheme.split(":")[0] in prefixes:
-            morpheme_seq = [info_morpheme] + morpheme_seq
-        else:
-            morpheme_seq = morpheme_seq + [info_morpheme]
-
-    # print("lexeme ", lexeme)
-    # print("info_morpheme ", info_morphemes)
-    # print("Arranged ", morpheme_seq)
-
-    start = 0
-    root_not_found = False
-    post_root = False
-    for info_morpheme_idx in range(len(morpheme_seq)):
-        info_morpheme = morpheme_seq[info_morpheme_idx]
-        is_last = info_morpheme_idx == len(morpheme_seq)-1
-
-        is_root = False
-        is_prefix = False
-
-        if info_morpheme.split(":")[0] == "conversion":
-            features = {"type":"conversion"}
-            if info_morpheme.endswith("-p"):
-                features["order_info"] = "simultaneous"
-                info_morpheme = info_morpheme[:-2]
-            if info_morpheme.endswith("-g"):
-                features["order_info"] = "undecided"
-                info_morpheme = info_morpheme[:-2]
-            features["conv_type"] = info_morpheme.split(":")[1]
-
-            lexicon.add_contiguous_morpheme(lex_id, annot_name, start, start, features)
-
-            continue
-
-        if len(info_morpheme.split(":")) < 2:
-            seg_issues.warning("Empty morph information %s of lexeme %s with desc %s", info_morpheme, lexeme, line)
-            continue
-
-        if info_morpheme.split(":")[1] in root_types:
-            is_root = True
-
-        if info_morpheme.split(":")[0] == "baseless" or info_morpheme.split(":")[1] == "suppl":
-            root_not_found = True
-            continue
-
-        if is_root:
-            allomorph = info_morpheme.split(":")[0]
-        else:
-            allomorph = info_morpheme.split(":")[1]
-
-        morpheme = info_morpheme.split(":")[0]
-        if morpheme in prefixes:
-            is_prefix = True
-
-        current_allomorph_set = {allomorph}
-        if morpheme in allomorph_set:
-            current_allomorph_set = current_allomorph_set.union(allomorph_set[morpheme])
+            # if info_morpheme.split(":")[0] == "conversion":
+                # conversion_lengths.append(sum([len(m.split(":")[1]) for m in morpheme_seq]))
 
 
-        morph_start, morph_end = choose_allomorph_boundaries(lexeme, current_allomorph_set, start, is_last, is_prefix)
+        # print("lexeme ", lexeme)
+        # print("info_morpheme ", info_morphemes)
+        # print("morpheme_seq ", morpheme_seq)
+        # print("ordering ", ordering)
+        # # print("conv length ", conversion_lengths)
 
-        # print("New field: ")
-        # print(info_morpheme)
-        # print("Search with start: ", start)
-        # print(morpheme, allomorph, morph_start, morph_end)
+        start = 0
+        root_not_found = False
+        post_root = False
+        for info_morpheme_idx, info_morpheme in enumerate(morpheme_seq):
+            is_last = info_morpheme_idx == len(morpheme_seq)-1
 
-        #POSSIBLE ISSUES
-        if morph_start == -1:
-            if is_root:
+            is_root = False
+            is_prefix = False
+
+            order = ordering.pop(0)
+
+            if info_morpheme.split(":")[0] == "conversion":
+                # print(conversion_lengths, info_morpheme)
+                # length = conversion_lengths[0]
+                # conversion_lengths = conversion_lengths[1:]
+                features = {"type":"conversion", "ordering":order}
+                if info_morpheme.endswith("-p"):
+                    features["order_info"] = "simultaneous"
+                    info_morpheme = info_morpheme[:-2]
+                if info_morpheme.endswith("-g"):
+                    features["order_info"] = "undecided"
+                    info_morpheme = info_morpheme[:-2]
+                features["conv_type"] = info_morpheme.split(":")[1]
+
+                lexicon.add_contiguous_morpheme(lex_id, annot_name, start, start, features)
+
+                continue
+
+            if len(info_morpheme.split(":")) < 2:
+                seg_issues.warning("Empty morph information %s of lexeme %s with desc %s", info_morpheme, lexeme, line)
+                continue
+
+            if info_morpheme.split(":")[1] in root_types:
+                is_root = True
+
+            if info_morpheme.split(":")[0] == "baseless" or info_morpheme.split(":")[1] == "suppl":
                 root_not_found = True
-            seg_issues.warning("Morph %s not found in lexeme %s with entry %s", allomorph, lexeme, line)
-            continue
-        if morph_end <= start:
-            seg_issues.warning("Morph %s contained in previous morph in lexeme %s with entry %s", allomorph, lexeme, line)
-        if morph_start < start:
-            if allomorph.startswith(lexeme[morph_start:start]):
-                boundary_overlap_issues.warning("Morph %s overlaps with previous morpheme in lexeme %s with entry %s", allomorph, lexeme, line)
-        if morph_start == morph_end:
-            seg_issues.warning("Empty morph %s in lexeme %s with entry %s", allomorph, lexeme, line)
-            continue
+                continue
+
+            if is_root:
+                allomorph = info_morpheme.split(":")[0]
+            else:
+                allomorph = info_morpheme.split(":")[1]
+
+            morpheme = info_morpheme.split(":")[0]
+            if morpheme in prefixes:
+                is_prefix = True
+
+            current_allomorph_set = {allomorph}
+            if morpheme in allomorph_set:
+                current_allomorph_set = current_allomorph_set.union(allomorph_set[morpheme])
 
 
-        #ADD INTERFIX/STEM IF REQUIRED
-        if morph_start > start:
-            if root_not_found:
-                #ADD WARNING HERE?
-                features = {"type":"root"}
-                features["morpheme"] = lex_features["root"]
+            morph_start, morph_end = choose_allomorph_boundaries(lexeme, current_allomorph_set, start, is_last, is_prefix)
+
+            # print("New field: ")
+            # print(info_morpheme)
+            # print("Search with start: ", start)
+            # print(morpheme, allomorph, morph_start, morph_end)
+
+            #POSSIBLE ISSUES
+            if morph_start == -1:
+                if is_root:
+                    root_not_found = True
+                seg_issues.warning("Morph %s not found in lexeme %s with entry %s", allomorph, lexeme, line)
+                continue
+            if morph_end <= start:
+                seg_issues.warning("Morph %s contained in previous morph in lexeme %s with entry %s", allomorph, lexeme, line)
+            if morph_start < start:
+                if allomorph.startswith(lexeme[morph_start:start]):
+                    boundary_overlap_issues.warning("Morph %s overlaps with previous morpheme in lexeme %s with entry %s", allomorph, lexeme, line)
+            if morph_start == morph_end:
+                seg_issues.warning("Empty morph %s in lexeme %s with entry %s", allomorph, lexeme, line)
+                continue
+
+            #CHECK FOR DOUBLING
+            doubling = False
+            if morpheme == "acons":
+                if lexeme[morph_end] == lexeme[morph_end+1]:
+                    morph_end = morph_end+1
+                    doubling = True
+
+            #ADD INTERFIX/STEM IF REQUIRED
+            if morph_start > start:
+                if root_not_found:
+                    #ADD WARNING HERE?
+                    features = {"type":"root", "ordering":0}
+                    features["morpheme"] = lex_features["root"]
+                    if "root_type" in lex_features:
+                        features["root_type"] = lex_features["root_type"]
+                    if "complex_type" in lex_features:
+                        features["complex_type"] = lex_features["complex_type"]
+                    lexicon.add_contiguous_morpheme(lex_id, annot_name, start, morph_start, features)
+                    root_not_found = False
+                elif post_root:
+                    lexicon.add_contiguous_morpheme(lex_id, annot_name, start, morph_start, features={"type":"part_of_root","ordering":0})
+                else:
+                    lexicon.add_contiguous_morpheme(lex_id, annot_name, start, morph_start, features={"type":"interfix","ordering":order})
+            #ADD MORPH
+            #Find features
+            features = {"morpheme":morpheme, "ordering":order}
+            if is_root == True:
+                features["type"] = "root"
                 if "root_type" in lex_features:
                     features["root_type"] = lex_features["root_type"]
                 if "complex_type" in lex_features:
                     features["complex_type"] = lex_features["complex_type"]
-                lexicon.add_contiguous_morpheme(lex_id, annot_name, start, morph_start, features)
-                root_not_found = False
-            elif post_root:
-                lexicon.add_contiguous_morpheme(lex_id, annot_name, start, morph_start, features={"type":"part_of_root"})
             else:
-                lexicon.add_contiguous_morpheme(lex_id, annot_name, start, morph_start, features={"type":"interfix"})
-        #ADD MORPH
-        #Find features
-        features = {"morpheme":morpheme}
-        if is_root == True:
-            features["type"] = "root"
-            if "root_type" in lex_features:
-                features["root_type"] = lex_features["root_type"]
-            if "complex_type" in lex_features:
-                features["complex_type"] = lex_features["complex_type"]
-        else:
-            if info_morpheme.endswith("-p"):
-                features["order_info"] = "simultaneous"
-                info_morpheme = info_morpheme[:-2]
-            if info_morpheme.endswith("-g"):
-                features["order_info"] = "undecided"
-                info_morpheme = info_morpheme[:-2]
+                if info_morpheme.endswith("-p"):
+                    features["order_info"] = "simultaneous"
+                    info_morpheme = info_morpheme[:-2]
+                if info_morpheme.endswith("-g"):
+                    features["order_info"] = "undecided"
+                    info_morpheme = info_morpheme[:-2]
 
-            if len(info_morpheme.split(":")) > 2:
-                features["mt"] = info_morpheme.split(":")[2][2:]
-            if len(info_morpheme.split(":")) > 3:
-                features["ms"] = info_morpheme.split(":")[3][2:]
+                if len(info_morpheme.split(":")) > 2:
+                    features["mt"] = info_morpheme.split(":")[2][2:]
+                if len(info_morpheme.split(":")) > 3:
+                    features["ms"] = info_morpheme.split(":")[3][2:]
 
-        lexicon.add_contiguous_morpheme(lex_id, annot_name, morph_start, morph_end, features)
+            if doubling:
+                features["doubling"] = True
 
-        # print(lexeme, start, end, allomorph)
-        start = max(start, morph_end)
+            lexicon.add_contiguous_morpheme(lex_id, annot_name, morph_start, morph_end, features)
 
-        #post_root only true if current morph was root, if not, it needs to be falsified
-        if is_root:
-            post_root = True
-        else:
-            post_root = False
-        #root_not_found only stays true for the immediately next morph, not after that
-        if root_not_found:
-            root_not_found = False
+            # print(lexeme, start, end, allomorph)
+            start = max(start, morph_end)
+
+            #post_root only true if current morph was root, if not, it needs to be falsified
+            if is_root:
+                post_root = True
+            else:
+                post_root = False
+            #root_not_found only stays true for the immediately next morph, not after that
+            if root_not_found:
+                root_not_found = False
+
+        #ADD FOR ALL UPOS
+        # curr_lexeme = lexicon.form(lex_id)
+        # curr_lemma = lexicon.lemma(lex_id)
+        # curr_features = lexicon.features(lex_id)
 
 
 outfile = open(sys.argv[3], 'w')
