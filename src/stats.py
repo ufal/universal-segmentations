bd5c6687252f33906ffa0d248fbb9f3ee77b93f3
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2022-01-07 10:37:14 +0100

    Merge the unit type with the size as kl / kf

--- src/stats.py
+++ src/stats.py
@@ -207,8 +207,7 @@ def process_file(filename):
 
     return (
         resource_name,
-        "l" if only_lemmas else "f",
-        round(segmented_lexeme_cnt / 1000),
+        "{:,} {}".format(round(segmented_lexeme_cnt / 1000), "kl" if only_lemmas else "kf"),
         #segmented_lexeme_cnt,
         #form_stats.type_count(),
         #lemma_stats.type_count(),
@@ -271,13 +270,13 @@ def main(args):
 
     if args.printer == "tex":
         if args.only == "both":
-            print(r"\begin{tabular}{llr|rrrrr|rrrrrrr} \toprule")
-            print(r"              & Seg. & Size      & \multicolumn{4}{c}{Histogram of morphs per unit [\%]} & Mean morphs & Mean unit & Mean morph & Morph  & Roots per & Prefixes & Suffixes \\")
-            print(r"Resource name & unit & [1000 u.] & 1 & 2 & 3 & 4+                                        & per unit    & length    & length     & avg. len & unit    & per unit & per unit \\ \midrule")
+            print(r"\begin{tabular}{lr|rrrrr|rrrrrrr} \toprule")
+            print(r"              &      & \multicolumn{4}{c}{Histogram of morphs per unit [\%]} & Mean morphs & Mean unit & Mean morph & Morph  & Roots per & Prefixes & Suffixes \\")
+            print(r"Resource name & Size & 1 & 2 & 3 & 4+                                        & per unit    & length    & length     & avg. len & unit    & per unit & per unit \\ \midrule")
         elif args.only == "left":
-            print(r"\begin{tabular}{llr|rrrrr|rr} \toprule")
-            print(r"              & Seg. & Size      & \multicolumn{4}{c}{Histogram of morphs per unit [\%]} & Mean morphs & Mean unit     & Mean morph \\")
-            print(r"Resource name & unit & [1000 u.] & 1 & 2 & 3 & 4+                                        & per unit    & length [char] & length [char] \\ \midrule")
+            print(r"\begin{tabular}{lr|rrrrr|rr} \toprule")
+            print(r"              &      & \multicolumn{4}{c}{Histogram of morphs per unit [\%]} & Mean morphs & Mean unit     & Mean morph \\")
+            print(r"Resource name & Size & 1 & 2 & 3 & 4+                                        & per unit    & length [char] & length [char] \\ \midrule")
         elif args.only == "right":
             print(r"\begin{tabular}{rrr} \toprule")
             print(r"Roots per & Prefixes & Suffixes \\")
@@ -287,10 +286,9 @@ def main(args):
         if args.only in {"left", "both"}:
             to_print += [
                 "Resource name",
-                "Segmented unit",
                 #"Lexemes",
                 #"Segmented lexemes",
-                "Size (thousands)", #"Forms",
+                "Size", #"Forms",
                 #"Lemmas",
                 #"POSes",
 
@@ -341,9 +339,9 @@ def main(args):
             if args.only == "both":
                 prn(*ret)
             elif args.only == "left":
-                prn(*ret[:10])
+                prn(*ret[:9])
             elif args.only == "right":
-                prn(*ret[10:])
+                prn(*ret[9:])
             else:
                 raise ValueError("Unknown `only` {}".format(args.only))
 
