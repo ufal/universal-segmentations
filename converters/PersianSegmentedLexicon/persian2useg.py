6b7c193d8d6d7f077ba6f23430b11ebdaa870a43
Honza <you@example.com>
2022-01-14 10:39:53 +0000

    PersianLexicon

--- converters/PersianSegmentedLexicon/persian2useg.py
+++ converters/PersianSegmentedLexicon/persian2useg.py
@@ -3,33 +3,41 @@
 import sys
 sys.path.append('../../src/')
 from useg import SegLex
-
+from morpheme_classifier import MorphemeClassifier
 
 import logging
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-if len(sys.argv) != 3:
-    sys.stderr.write("Usage:\n  "+__file__+" Persian-lex-file.txt converted-file.useg\n\n")
+if len(sys.argv) < 3:
+    sys.stderr.write("Usage:\n  "+__file__+" Persian-lex-file1.txt [Persian-lex-file2.txt] [Persian-lex-file3.txt] [...] converted-file.useg\n\n")
 
-logging.info(f"Converting {sys.argv[1]} to {sys.argv[2]}")
+logging.info(f"Converting {sys.argv[1:-1]} to {sys.argv[-1]}")
 
 lexicon = SegLex()
 
-infile = open(sys.argv[1])
+morpheme_cl=MorphemeClassifier()
+data=[]
+for file_name in sys.argv[1:-1]:
+    infile = open(file_name, "r", encoding="utf-8")
+    datafile_name=file_name.split("/")[-1].split(".")[0]
+    annot_name="PersianLexicon-"+datafile_name
+    for line in infile:
+        columns = line.rstrip().split(" ")
+        wordform = columns[0]
+        lemma = columns[1]
+        morphemes = columns[4:]
+        data.append([wordform, lemma, annot_name, morphemes])
+        morpheme_cl.Update(morphemes)
+    infile.close()
 
-for line in infile:
-    columns = line.rstrip().split(" ")
-    wordform = columns[0]
-    lemma = columns[1]
-    morphemes = columns[4:]
 
-    lexeme = lexicon.add_lexeme(wordform, lemma, '?TODO')
-
+for j,(wordform, lemma, annot_name, morphemes) in enumerate(data):
+    lexeme = lexicon.add_lexeme(wordform, lemma, pos="X")
+    morph_classes=morpheme_cl.Guess(morphemes) #workaround because of the right-to-left writing
     start = 0
-    for morpheme in morphemes:
-        lexicon.add_contiguous_morpheme(lexeme,'?TODO', start, start+len(morpheme)-1, features={"morpheme": morpheme, "type": "?TODO"})
-        start = start+len(morpheme)
-    
+    for i,morpheme in enumerate(morphemes):
+        lexicon.add_contiguous_morpheme(lexeme, annot_name, 0, 0, features={"morpheme": morpheme, "type": morph_classes[i]})
 
-outfile = open(sys.argv[2], 'w')
+outfile = open(sys.argv[-1], 'w', encoding="utf-8")
 lexicon.save(outfile)
+outfile.close()
